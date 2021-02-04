d001df756eb1a2d61e059db6f7efc07b50db3e5d
Th3At0nic <islammdrahatul@gmail.com>
2021-02-05 00:14:43 +0600

    added onclick

--- weather.js
+++ weather.js
@@ -1,17 +1,20 @@
-let cityName = document.getElementById("city-input").value;
+let cityInput = document.getElementById("city-input").value;
 let appid = "c8a02f0db1884b41d5e8fad78d640254";
 let url = "http://api.openweathermap.org/data/2.5/weather?q=";
 
-document.getElementById("search-btn").addEventListener("click", function () {
-    checkWeather();
-});
+// document.getElementById("search-btn").addEventListener("click", function () {
+//     checkWeather();
+// });
 
 function checkWeather() {
-    fetch(`${url}${cityName}&APPID=${appid}`)
+    fetch(`${url}${cityInput}&APPID=${appid}`)
         .then(response => response.json())
         .then(data => {
 
-            document.getElementById("city-name").innerText = cityName;
+            // const cityName = data.name;
+            // console.log(cityName);
+
+            document.getElementById("city-name").innerText = data.name;
             document.getElementById("temperature").innerText = (data.main.temp - 273.15).toFixed(2);
 
             document.getElementById("aerosol").innerText = data.weather[0].main;
